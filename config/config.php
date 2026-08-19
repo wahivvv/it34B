86e179b4f62886f4571166006ca800b7629ca28f
wahivvv <wahivwahiv4@gmal.com>
2026-08-19 16:01:34 +0800

    partial logging application with configuration and changes

--- config/config.php
+++ config/config.php
@@ -1,18 +1,19 @@
 <?php
-session start();
+session_start();
 
-define('BASE_URL', 'http://localhost/it34b/');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'it34b_lab_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('BASE_URL','http://localhost/it34b');
 
-try {
-    $pdo = new PDO(
-        "mysql:host" .DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS,
-        [PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION]
+define('DB_HOST','localhost');
+define('DB_NAME','it34b_lab_db');
+define('DB_USER','root');
+define('DB_PASS','');
+
+try{
+    $pdo =new PDO(
+        "mysql:host" . DB_HOST . ";dbname=" .DB_NAME, DB_USER, DB_PASS,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
-} catch (PDOException $e) {
+}catch(PDOException $e){
     die("Connection failed: " . $e->getMessage());
 }
 ?>

0a80f0ddb99fbc650b9a8f031925501e67499bfc
wahivvv <wahivwahiv4@agmail.com>
2026-08-26 15:48:13 +0800

    changing branches

--- it34B/db/schema.sql
+++ it34B/db/schema.sql
@@ -5,10 +5,10 @@
         activity_log_action VARCHAR(50) NOT NULL,
         activity_log_status ENUM('success','failed') DEFAULT 'success',
 
-        -- Client Parameters
+
         activity_log_ip_address VARCHAR(45),
         activity_log_user_agent VARCHAR(255),
         
-        -- Timestamp
+        
         activity_log_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );

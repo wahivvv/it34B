e087b5ce8f0b9553cac998026e2ea497ecfa16db
wahivvv <wahivwahiv4@gmail.com>
2026-09-14 15:32:11 +0800

    added logs query & admin/index

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,12 +1,67 @@
+<?php
+require '../../config/config.php';
+require '../../config/functions.php';
+
+requireRole('admin');
+
+logActivity(
+    $pdo,
+    $_SESSION['user_id'],
+    $_SESSION['user_email'],
+    'view_activity_logs',
+    'success'
+);
+
+// Activity Logs Query #3
+$stmt = $pdo->query("
+    SELECT *
+    FROM activity_logs
+    ORDER BY activity_log_created_at DESC
+");
+
+$activities = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome Admin</title>
+    <h1>CREATED BY: RENE BATERBONIA "MVP"<h1>
+
 </head>
 <body>
-    <h1>Welcome Admin<h1>
+    <h1>Welcome Admin</h1>
     <a href="../../auth/signout.php">Sign Out</a>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Record ID</th>
+                <th>User ID</th>
+                <th>User Email</th>
+                <th>Action</th>
+                <th>Status</th>
+                <th>IP Address</th>
+                <th>User Agent</th>
+                <th>Date & Time</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($activities as $activity):?>
+                <tr>
+                    <td><?= htmlspecialchars($activity['activity_log_id'])?></td>
+                    <td><?= htmlspecialchars($activity['user_id'])?></td>
+                    <td><?= htmlspecialchars($activity['user_email'])?></td>
+                    <td><?= htmlspecialchars($activity['activity_log_action'])?></td>
+                    <td><?= htmlspecialchars($activity['activity_log_status'])?></td>
+                    <td><?= htmlspecialchars($activity['activity_log_ip_address'])?></td>
+                    <td><?= htmlspecialchars($activity['activity_log_user_agent'])?></td>
+                    <td><?= htmlspecialchars($activity['activity_log_created-at'])?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>

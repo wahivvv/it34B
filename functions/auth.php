92796d4a79594715463ff0545577ecc1b6935739
wahivvv <wahivwahiv4@gmail.com>
2026-09-23 15:45:35 +0800

    StartUserSession

--- functions/auth.php
+++ functions/auth.php
@@ -32,7 +32,7 @@ function loginUser($pdo, $login, $password)
     $_SESSION['user_email'] = $user['user_email'];
     $_SESSION['user_username'] = $user['user_username'];
     $_SESSION['user_role'] = $user['user_role'];
-
+    $_SESSION['session_id'] = startUserSession($pdo);
     return true;
 }
 
